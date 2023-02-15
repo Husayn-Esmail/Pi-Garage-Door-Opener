283050a7654d78ff9077e06cae4ab9df756479d0
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2023-02-15 16:25:42 -0700

    removed code for email which no longer works due to a google change

--- irsensor.py
+++ irsensor.py
@@ -12,10 +12,8 @@ it's state and returns a true or a flase value which can be
 passed in via a get request. '''
 import RPi.GPIO as GPIO
 import time
-import ssl
-import smtplib
 
-print("working") # for log/debug purposes
+print("irsensor being called") # for log/debug purposes
 # Init GPIOs
 # set GPIO pin mode
 GPIO.setmode(GPIO.BCM)
@@ -26,44 +24,6 @@ GPIO.setup(16, GPIO.IN)
 
 # start the sensor
 GPIO.output(20, GPIO.HIGH)
-
-# init smtp server and send email
-def sendEmail():
-	''' sends emails/text messages when called '''
-	port = 465 # for ssl
-	smtpServer = "smtp.gmail.com"
-	senderEmail = "email@example.com"
-	password = "password"
-	message = """ATTENTION: The garage door has been open for 15 minutes, if this is not intentional, please close the door. """
-	# create a secure ssl context
-	context = ssl.create_default_context()
-	
-	# mail recievers
-	# person name
-	reciever1 = "phonenumber@carrier.email.com"
-	with smtplib.SMTP_SSL(smtpServer, port, context=context) as server:
-		server.login(senderEmail, password)
-		# send emails
-		server.sendmail(senderEmail, receiver1, message)
-
-# starts a counter if the sensor detects it's open
-def countIfOpen():
-	# read data from the sensor returns 1 or 0 (1=not triggered, 0 = triggerd)
-	data = GPIO.input(16)
-	seconds = 0
-	baseValue = 900
-	# while triggered
-	while data = 0:
-		# start timer
-		time.sleep(1)
-		seconds += 1
-		# to make sure data is updated and prevent an infinite loop
-		data = GPIO.input(16)
-		print('seconds', seconds)
-		while seconds >= baseValue:
-			sendEmail()
-			print("completed")
-			baseValue += 900
 
 def get_status():
 	''' returns the state of the garage opener
@@ -81,7 +41,7 @@ def get_status():
 if __name__ == '__main__':
 	continueProgram = True
 	while continueProgram:
-		countIfOpen()
+		print(get_status())
 		time.sleep(1)
 
 # clean up pin
